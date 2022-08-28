ebd7a990c621426e15e8fa66905a6e4136886ff2
lucUG <lamtuanluc04112002@gmail.com>
2022-08-28 09:52:12 +0700

    Refactor code: create routes, app/controllers folders

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,16 @@
+
+
+class NewsController {
+
+    //[GET] /news
+    index(req, res) {
+        res.render('news');
+    }
+
+    //[GET] /news/:slug
+    show(req, res) {
+        
+    }
+}
+
+module.exports = new NewsController;
